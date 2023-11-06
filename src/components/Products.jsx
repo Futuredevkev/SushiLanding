c40c780a6aeb911774a5fd217aa4d7e3419dabe0
Futuredevkev <kevinsitosoyyoguadateamo1212@outlook.com>
2023-11-06 18:56:39 -0300

    2

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import CartContext from "../Context/CartContext";
 
 export function Products({ products }) {
